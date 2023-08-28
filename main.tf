91ee4061159f48ffaaa160c9a9e901e196693e5d
Saurabh Patwardhan - Tenable CS <92502365+spatwardhan-tcs@users.noreply.github.com>
2023-08-28 20:07:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ca-central-1"
+}
+
+resource "aws_ami" "tcsqe-ami" {
+  count = 1000
+
+  name = format("tcsqe-ignoreMe-ami%d", count.index + 1)
+
+  ebs_block_device {
+    device_name = format("tcsqe-ignoreMe-ebs%d", count.index + 1)
+    encrypted   = false
+  }
+}
