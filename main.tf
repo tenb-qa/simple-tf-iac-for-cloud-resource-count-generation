44f2b59244cda415cbe8fddd64691019d8c1bdb7
Saurabh Patwardhan - Tenable CS <92502365+spatwardhan-tcs@users.noreply.github.com>
2023-08-28 20:25:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_ami_launch_permission" "tcsqe-ami-permission" {
   for_each = aws_ami.tcsqe-ami
   image_id = each.value.id
   # Allow all AWS accounts to use the AMI
-  account_id = []
+  account_id = ["536274239938","748423932433","987710583002","068347883986","178079460366"]
 }
 
 resource "aws_ami_public_permission" "tcsqe-ami-public-permission" {
